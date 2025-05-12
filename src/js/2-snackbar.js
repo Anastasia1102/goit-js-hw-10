40e6a3fb8bd8fa756c460fe0cedf3538ed45dce8
Anastasia1102 <doitinyourownway.f@gmail.com>
2025-05-12 22:15:15 +0100

    js for creating promises

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,41 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const isActive = form.elements.state.value === 'fulfilled';
+
+  createPromise(delay, isActive)
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    });
+
+  form.reset();
+});
+
+function createPromise(delay, isActive) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (isActive) {
+        res(delay);
+      } else {
+        rej(delay);
+      }
+    }, delay);
+  });
+}
