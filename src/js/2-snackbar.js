ded6b39abca43671686367d65b888d71b19cc745
Anastasia1102 <doitinyourownway.f@gmail.com>
2025-05-15 22:02:24 +0100

    import css

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,6 +1,8 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
+import '../css/2-snackbar.css';
+
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', event => {
